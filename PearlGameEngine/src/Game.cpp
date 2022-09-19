d5d44c16b592756dbe45358ec81bedcc8270cd70
maxvone <maximvone@gmail.com>
2022-09-19 19:16:44 +0200

    add: create an SDL window

--- PearlGameEngine/src/Game.cpp
+++ PearlGameEngine/src/Game.cpp
@@ -1,9 +1,9 @@
 #include "Game.h"
-#include <SDL.h>
 #include <iostream>
 
 Game::Game()
 {
+	isRunning = false;
 	std::cout << "Game constructor call" << std::endl;
 }
 
@@ -20,33 +20,28 @@ void Game::Initialize()
 		return;
 	}
 
-	SDL_Window* window = SDL_CreateWindow(
-		NULL,
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
-		SDL_WINDOW_BORDERLESS
-	);
+	window = SDL_CreateWindow(NULL,	SDL_WINDOWPOS_CENTERED,	SDL_WINDOWPOS_CENTERED,
+		800, 600, SDL_WINDOW_BORDERLESS);
+
 	if (!window)
 	{
 		std::cerr << "Error creating sdl window." << std::endl;
 		return;
 	}
 
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	renderer = SDL_CreateRenderer(window, -1, 0);
 	if (!renderer)
 	{
-
 		std::cerr << "Error creating sdl renderer." << std::endl;
 		return;
 	}
 
+	isRunning = true;
 }
 
 void Game::Run()
 {
-	while (true)
+	while (isRunning)
 	{
 		ProcessInput();
 		Update();
@@ -56,16 +51,42 @@ void Game::Run()
 
 void Game::ProcessInput()
 {
+	SDL_Event sdlEvent;
+	while (SDL_PollEvent(&sdlEvent))
+	{
+		switch (sdlEvent.type)
+		{
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			case SDL_KEYDOWN:
+				if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
+				{
+					isRunning = false;
+				}
+				break;
+		}
+	}
 }
 
 void Game::Update()
 {
+	//TODO: Update game objects state
 }
 
 void Game::Render()
 {
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	//TODO: Render all game objects
+	
+	SDL_RenderPresent(renderer);
 }
 
 void Game::Destroy()
 {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
--- PearlGameEngine/src/Game.h
+++ PearlGameEngine/src/Game.h
@@ -1,13 +1,18 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <SDL.h>
+
 class Game{
+
 private:
-
-
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 public:
 	Game();
 	~Game();
+
 	void Initialize();
 	void Run();
 	void ProcessInput();
